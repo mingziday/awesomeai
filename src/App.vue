b4834a71bc6ff0706a5603f49eaa067779f60162
mingziday <mingziday@126.com>
2023-12-19 00:14:57 +0800

    setup gh-dpages

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did itooooo!" />helpesds
+      <HelloWorld msg="You did itooooovvvv!" />helpesds
     </div>
   </header>
 
